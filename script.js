893acb2e699d5cce2ba3ccaf1e15915e72822c8f
Orxan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-10-12 13:50:52 +0400

    alqoritmlar

---
+++ script.js
@@ -0,0 +1,214 @@
+// let num=parseInt(prompt("yasinizi daxil edin"))
+// // num 66
+// if (num>0){
+//     if (num<125){
+//         if(num>65){
+//             console.log("yasiniz uygundur");
+
+//         }else{
+//             console.log("yasiniz uygun deyil");
+//         }
+
+//     }else{
+//         console.log("duzgun yas daxil edin");
+//     }
+// }else{
+//     console.log("musbet eded daxil edin");
+// }
+
+// let a=parseInt(prompt("teref a ni qeyd edin"))
+// let b=parseInt(prompt("teref b ni qeyd edin"))
+// let c=parseInt(prompt("teref c ni qeyd edin"))
+// if (a > 0 && b > 0 && c > 0){
+//     if (a == b && a == b && b == c){
+//      console.log("beraberterefli");
+//     }else if (a !=0 && b !=0 && c !=0){
+//         console.log("muxtelifterefli");
+//     }else if (a == b || a == c || b == c){
+//         console.log("beraberyanli");
+
+//     }
+// }else{
+//     console.log("menfi eded var");
+// }
+
+// let number=7
+
+// if (number <= 1) {
+//   console.log(number + " ne murekkeb ne de sade ededdir");
+// } else if (number <= 7) {
+//   console.log(number + " sade ededdir");
+// } else if (number % 2 === 0 || number % 3 === 0 || number % 5 === 0  || number % 7 === 0) {
+//   console.log(number + " murekkeb ededdir");
+// } 
+// else{
+//     console.log(number + " sade ededdir");
+//   }
+
+
+// let obj={a:1,b:2,c:3,d:4}
+
+// for (const element in obj){
+//     console.log(element,obj.value);
+// }
+// stringin bosluqlrini silmek
+// let words="Salamlar Necesiniz?Nevar neyox"
+// let newWords='';
+// for (let i = 0; i < words.length; i++) {
+
+//     if (words[i] !== ' ' ) {
+//         newWords += words[i]
+//     }
+// }
+// 1cu -------------------
+// let a=10
+// let b=12
+// let c=14
+// if (a + b < c || a + c < b || c + b < a  ) {
+//     console.log("ucbucagin terefleri deil");
+// }
+// else{
+//     console.log("ucbucagin terefleri ");
+// }
+// ==============================
+
+
+// 2ciiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+// let a=12
+//  let b=12
+// let c=12
+// if (a===b && a===c && b===c ) {
+//     console.log("beraber tereflidir");
+// }
+// else{
+//     console.log("beraber terefli deil");
+// }
+// --------------------------------------------
+
+// 3cuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+// let a=12
+//  let b=12
+// let c=25
+
+//    if (a===b || a===c || b===c) {
+//     console.log("BERABERYANLI");
+// }  
+
+// else{
+//     console.log("BERABERYANLI deyil");
+// }
+// =============================================
+// 4cuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+// let a=12
+//  let b=12
+// let c=25
+// if (a**2 + b**2 === c**2 ||a**2 + c**2 === b**2 || c**2 + b**2 === a**2  ) {
+//          console.log("Duzbucaq ");
+//     }  
+
+//     else{
+//          console.log("Duzbucaq deyil");
+//      }
+//      ==========================================
+// 5ciiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+// let 
+// ????????????????????????????????????????????????????
+// 666666666666666666666666666666666666666666666666666
+// let i=50 
+// for (let i = 0; i < 50; i++) {
+//  if (i%3===0 && i%2===0) {
+//     console.log(i);
+//  }
+// }
+// -------------------------------------------------
+// 77777777777777777777777777777777777777777
+// let i = 100
+// for (let i = 0; i < 100; i++) {
+//     if (i > 10 && i % 11 === 0) {
+
+//         console.log(i);
+
+//     }
+
+// }
+// =====================================
+// 8888888888888888888888888888888888888888888
+// let i = 100
+// for (let i = 0; i < 100; i++) {
+//     if (i > 10 && i % 11 !== 0) {
+
+//         console.log(i);
+
+//     }
+
+// }
+// ===============================================
+// 99999999999999999999999999999999999999999999999999
+// for (let j = 1; j < 10; j++) {
+    
+
+//     for (let p = 1; p < 10; p++) {
+        
+//         console.log(j*p)
+//     }
+// }
+// =======================================
+// 100000000000000000000000000000000000000000000
+// let factorial=1
+// let num=5
+// for (let i = 1; i <= 5; i++) {
+//     factorial *=i
+   
+// }
+
+// console.log(factorial)
+// ========================================
+// 12121222222222222222222222222222222222
+// let a = 1234  + ""
+// let yaddas = ""
+// for (i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+//     yaddas += " "+ a[i]
+// }
+// console.log(yaddas);
+// ==========================
+
+// 13131331313131313131313
+
+
+// =========================
+// 1414141441414414
+// let number = 56627; 
+
+// let numberStr = number.toString();
+// let minDigit = 9;
+
+// for (let i = 0; i < numberStr.length; i++) {
+//   let digit = parseInt(numberStr[i]);
+//   if (digit < minDigit) {
+//     minDigit = digit;
+//   }
+// }
+// console.log(minDigit);
+// ======================
+// 151515151551515515155115
+// let num=243
+// let arr= num + ""
+// let cem=0 
+// let hasil=1
+// for (let i = 0; i < arr.length; i++) {
+//    cem += +arr[i]
+//    hasil *= +arr[i]
+//    orta=cem/arr.length
+    
+// }
+
+// console.log(cem); 
+// 1616161661616616161661616
+// let eded=24
+// for (let i = 0; i < 24; i++) {
+//    if(eded%i===0){
+//     console.log(i);
+//    }  
+// }
+// ================================
